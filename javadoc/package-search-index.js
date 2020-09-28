8515d78c4a834035378a76464a790adc8e29c89d
Alexandra Maliauka <maliaukas@gmail.com>
2020-09-28 14:34:26 +0300

    Add javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"model.builder"},{"l":"model.coffee"},{"l":"model.coffee_enums"},{"l":"model.comparators"},{"l":"view"}]
